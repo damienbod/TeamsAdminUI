ad6e9250179d25f90b7703928f5639de42406d49
damienbod <damien_bod@hotmail.com>
2021-09-17 18:24:50 +0200

    init project

---
+++ TeamsAdminUI/MailSender/AadGraphApiDelegatedClient.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Graph;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace TeamsAdminUI.MailSender
+{
+    public class AadGraphApiDelegatedClient
+    {
+        private readonly GraphServiceClient _graphServiceClient;
+
+        public AadGraphApiDelegatedClient(GraphServiceClient graphServiceClient)
+        {
+            _graphServiceClient = graphServiceClient;
+        }
+
+        public async Task SendEmailAsync(Message message)
+        {
+            var saveToSentItems = true;
+
+            await _graphServiceClient.Me
+                .SendMail(message, saveToSentItems)
+                .Request()
+                .PostAsync();
+        }
+
+    }
+}
---
+++ TeamsAdminUI/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Identity.Web;
+using System.Threading.Tasks;
+using TeamsAdminUI.MailSender;
+
+namespace TeamsAdminUI.Pages
+{
+    public class CallApiModel : PageModel
+    {
+        private readonly AadGraphApiDelegatedClient _aadGraphApiDelegatedClient;
+
+        public CallApiModel(AadGraphApiDelegatedClient aadGraphApiDelegatedClient)
+        {
+            _aadGraphApiDelegatedClient = aadGraphApiDelegatedClient;
+        }
+
+        public void OnGet()
+        {
+           
+        }
+    }
+}
